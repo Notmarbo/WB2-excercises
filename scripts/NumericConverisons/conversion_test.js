ee7d753cf62e53f13d78277c82d2a55712cbbe20
Marco Sanchez <Notmarcosan@gmail.com>
2023-04-25 16:01:01 -0400

    Completed 2-33

---
+++ scripts/NumericConverisons/conversion_test.js
@@ -0,0 +1,27 @@
+//decription: THis script test various numeric conversion techniques
+// Author: Msrco Sanchez
+"use strict"
+
+// var a = parseInt("101.1");
+// var b = parseInt("55");
+// var c = parseInt("402 Stevens");
+// var d = parseInt("Number 5");
+// console.log(a, b, c, d);
+
+// var a1 = parseFloat("101.1");
+// var b1 = parseFloat("55");
+// var c1 = parseFloat("402 Stevens");
+// var d1 = parseFloat("Number 5");
+// console.log(a1, b1, c1, d1);
+
+// var a2 = Number("101.1");
+// var b2 = Number("55");
+// var c2 = Number("402 Stevens");
+// var d2 = Number("Number 5");
+// console.log(a2, b2, c2, d2);
+
+var a = +("101.1");
+var b = +("55");
+var c = +("402 Stevens");
+var d = +("Number 5");
+
